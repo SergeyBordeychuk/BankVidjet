70525b38d27331f5e8997b6169389d51e928d5f0
Sergey Bordeychuk <twnue2020@gmail.com>
2025-01-19 15:46:51 +0300

    add func get_date and fixes

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,12 @@
-def mask_account_card(card) -> str:
-    if 'Счет' in card:
-        return f'Счет **{card[-4:]}'
-    else:
-        return f'{card[:-17]} {card[-17:-12]} {card[-12:-10]}** **** {card[-4:]}'
+from typing import Union
 
-print(mask_account_card('Visa Platinum 7000792289606361'))
+
+def mask_account_card(card: Union[str]) -> str:
+    if "Счет" in card:
+        return f"Счет **{card[-4:]}"
+    else:
+        return f"{card[:-17]} {card[-17:-12]} {card[-12:-10]}** **** {card[-4:]}"
+
+
+def get_date(date: Union[str]) -> str:
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
