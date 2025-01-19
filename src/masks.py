96fa913edc466648cbd7b5555d10a43936e21a68
Sergey Bordeychuk <twnue2020@gmail.com>
2025-01-19 15:03:01 +0300

    fixing the project

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def get_mask_card_number(card: Union[int, str]) -> str:
+    """Создаём маску карты"""
+    card = str(card)
+    card_mask = card[:6] + "******" + card[12:]
+    return card_mask[:4] + " " + card_mask[4:8] + " " + card_mask[8:12] + " " + card_mask[12:]
+
+
+def get_mask_account(account: Union[int, str]) -> str:
+    """Создаём маску счёта"""
+    account = str(account)
+    account_mask = "**" + account[-4:-1]
+    return account_mask
