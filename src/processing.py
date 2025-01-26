cb8f5f9fe586debb258e5e3bd8435ba9dfd6959d
Sergey Bordeychuk <twnue2020@gmail.com>
2025-01-26 19:01:11 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,17 @@ def filter_by_state(list_get: Union[list], state: Union[str] = "EXECUTED") -> li
             if value == state:
                 new_list.append(dict)
     return new_list
+
+
+def sort_by_date(list_get: Union[list], revers: Union[bool] = True) -> list:
+    """Возвращаеь словари отсортированные по датам"""
+    new_list = []
+    dates = []
+    for dictionary in list_get:
+        dates.append(dictionary.get("date"))
+        dates = sorted(dates, reverse=revers)
+    for date in dates:
+        for diction in list_get:
+            if date == diction.get("date"):
+                new_list.append(diction)
+    return new_list
