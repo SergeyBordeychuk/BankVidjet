549649e8a287d1c4572a434a7fe3e617a14a65db
Sergey Bordeychuk <twnue2020@gmail.com>
2025-01-26 20:57:44 +0300

    update func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -13,13 +13,5 @@ def filter_by_state(catalogue: Union[list], state: Union[str] = "EXECUTED") -> l
 
 def sort_by_date(catalogue: Union[list], revers: Union[bool] = True) -> list:
     """Возвращаеь словари отсортированные по датам"""
-    new_list = []
-    dates = []
-    for dictionary in catalogue:
-        dates.append(dictionary.get("date"))
-        dates = sorted(dates, reverse=revers)
-    for date in dates:
-        for diction in catalogue:
-            if date == diction.get("date"):
-                new_list.append(diction)
-    return new_list
+    sorted_list = sorted(catalogue, key=lambda x: x['date'], reverse=revers)
+    return sorted_list
