980cd78147913e05155d718f248353b608ccf4bf
Sergey Bordeychuk <twnue2020@gmail.com>
2025-01-27 14:21:36 +0300

    fix filter_by_state and annotation

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,13 @@
-from typing import Union
-
-
-def filter_by_state(catalogue: Union[list], state: Union[str] = "EXECUTED") -> list:
+def filter_by_state(catalogue: list, state: str = "EXECUTED") -> list:
     """Возвращает словари с ключом state"""
     sorted_list = []
     for dictionary in catalogue:
-        for value in dictionary.values():
-            if value == state:
-                sorted_list.append(dictionary)
+        if dictionary.get("state") == state:
+            sorted_list.append(dictionary)
     return sorted_list
 
 
-def sort_by_date(catalogue: Union[list], revers: Union[bool] = True) -> list:
+def sort_by_date(catalogue: list, revers: bool = True) -> list:
     """Возвращаеь словари отсортированные по датам"""
     sorted_list = sorted(catalogue, key=lambda x: x["date"], reverse=revers)
     return sorted_list
