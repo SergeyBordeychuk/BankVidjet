c949ab5e25d1f16179c969a5262b548f42d5cc78
Sergey Bordeychuk <twnue2020@gmail.com>
2025-02-08 19:46:01 +0300

    add tests

--- src/widget.py
+++ src/widget.py
@@ -6,7 +6,7 @@ def mask_account_card(card: Union[str]) -> str:
     if "Счет" in card:
         return f"Счет **{card[-4:]}"
     else:
-        return f"{card[:-17]} {card[-17:-12]} {card[-12:-10]}** **** {card[-4:]}"
+        return f"{card[:-17]}{card[-17:-12]} {card[-12:-10]}** **** {card[-4:]}"
 
 
 def get_date(date: Union[str]) -> str:
