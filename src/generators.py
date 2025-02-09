31f479f9222f8746df4c4b5df51824e1e8b05006
Sergey Bordeychuk <twnue2020@gmail.com>
2025-02-09 20:42:30 +0300

    new modele and 3 func

---
+++ src/generators.py
@@ -0,0 +1,15 @@
+def filter_by_currency(transitions, currency):
+        for transition in transitions:
+            if ((transition.get('operationAmount')).get('currency')).get('code') == currency:
+                yield transition
+
+
+def transaction_descriptions(transitions):
+    for transition in transitions:
+        yield transition.get('description')
+
+
+def card_number_generator(first_number, last_number):
+    for number in range(first_number, last_number+1):
+        card_number = '0' * (16-len(str(number))) + str(number)
+        yield f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:17]}'
