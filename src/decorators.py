438db5b4916153c66c8b01d4359dd3ff0b6e2986
Sergey Bordeychuk <twnue2020@gmail.com>
2025-02-20 22:34:22 +0300

    update fixes

--- src/decorators.py
+++ src/decorators.py
@@ -8,25 +8,28 @@ def log(filename=None):
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-            global result_func
             if filename != None:
                 logging.basicConfig(level=logging.INFO, filename=filename, filemode="a")
                 logging.info(f"Name function: {str(func)[10:-23]}")
                 try:
-                    func(*args, **kwargs)
-                    return logging.info(f"ok")
+                    result_func = func(*args, **kwargs)
+                    logging.info(f"ok")
+                    return result_func
                 except Exception as e:
-                    return logging.error(f"error: {e}. Inputs: {args} {kwargs}")
+                    logging.error(f"error: {e}. Inputs: {args} {kwargs}")
+                    return
             else:
                 result = ""
                 result += f"Name func: {str(func)[10:-23]}\n"
                 try:
                     result_func = func(*args, **kwargs)
                     result += f"ok"
+                    print(result)
+                    return result_func
                 except Exception as e:
                     result += f"error: {e}. Inputs: {args} {kwargs}"
-                print(result)
-                return result_func
+                    print(result)
+                    return
 
         return wrapper
 
