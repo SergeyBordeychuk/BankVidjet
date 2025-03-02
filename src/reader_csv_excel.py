7cd9e447f7372d6f5305314b030e662a17175c65
Sergey Bordeychuk <twnue2020@gmail.com>
2025-03-02 16:10:13 +0300

    add module reader_csv_excel.py and 2 func

---
+++ src/reader_csv_excel.py
@@ -0,0 +1,20 @@
+import csv
+
+import pandas as pd
+from pandas.core.interchange.dataframe_protocol import DataFrame
+
+
+def reader_csv(path: str) -> list:
+    """Функция считывает финансовые операции csv файлов"""
+    file_result = []
+    with open(path) as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            file_result.append(row)
+        return file_result
+
+
+def reader_excel(path: str) -> DataFrame:
+    """Функция считывает финансовые операции excel файлов"""
+    excel_file = pd.read_excel(path)
+    return excel_file
