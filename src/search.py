b95f7021f18a9c20410ae3ba9fafee34be745e41
Sergey Bordeychuk <twnue2020@gmail.com>
2025-03-04 19:57:11 +0300

    fix search_operations

--- src/search.py
+++ src/search.py
@@ -4,7 +4,8 @@ import re
 def search_operations(list_operations:list, str_search:str) -> list:
     list_dict = []
     for operation in list_operations:
-        operation_find = re.search(str_search, operation['description'], flags=0)
-        if operation_find:
-            list_dict.append(operation)
+        if operation != {}:
+            operation_find = re.search(str_search, operation['description'], flags=0)
+            if operation_find:
+                list_dict.append(operation)
     return list_dict
