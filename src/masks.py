f0548d02993fa9364b35bf17f90dddf28152235f
Sergey Bordeychuk <twnue2020@gmail.com>
2025-03-07 20:31:48 +0300

    fix tests

--- src/masks.py
+++ src/masks.py
@@ -12,22 +12,26 @@ logger.addHandler(file_handler)
 def get_mask_card_number(card: Union[int, str]) -> str:
     """Создаём маску карты"""
     logger.info(f"Начало маскировки номера карты: {card}")
+    card = str(card)
     if card[0].isalpha() == 1:
         name_card = card[:-17]
-        card = str(card)[-16:]
+        card = card[-16:]
         card_mask = card[:6] + "******" + card[12:]
         card_mask_finally = card_mask[:4] + " " + card_mask[4:8] + " " + card_mask[8:12] + " " + card_mask[12:]
         return f'{name_card} {card_mask_finally}'
     else:
-        card = str(card)
-    card_mask = card[:6] + "******" + card[12:]
-    card_mask_finally = card_mask[:4] + " " + card_mask[4:8] + " " + card_mask[8:12] + " " + card_mask[12:]
-    return f'{card_mask_finally}'
+        card_mask = card[:6] + "******" + card[12:]
+        card_mask_finally = card_mask[:4] + " " + card_mask[4:8] + " " + card_mask[8:12] + " " + card_mask[12:]
+        return f'{card_mask_finally}'
 
 
 def get_mask_account(account: Union[int, str]) -> str:
     """Создаём маску счёта"""
     logger.info(f"Начало маскировки счёта: {account}")
     account = str(account)
-    account_mask = "Счёт" + " " + "**" + account[-4:]
-    return account_mask
+    if 'Счёт' in account:
+        account_mask = "Счёт" + " " + "**" + account[-4:]
+        return account_mask
+    else:
+        account_mask ="**" + account[-4:]
+        return account_mask
