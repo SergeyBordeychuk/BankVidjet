521110991cd98a63eb3535114bf759eb96af2205
Sergey Bordeychuk <twnue2020@gmail.com>
2025-03-07 07:26:15 +0300

    fix masks and extend_apu

--- src/external_api.py
+++ src/external_api.py
@@ -10,7 +10,6 @@ API_KEY = os.getenv("API_KEY")
 def amount_transition(transition: dict) -> float:
     """Фукция обрабатывает транзакцию и возвращает её сумму в рублях"""
     code = transition["operationAmount"]["currency"]["code"]
-    print(code)
     if code != "RUB":
         amount = transition["operationAmount"]["amount"]
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}"
@@ -19,8 +18,6 @@ def amount_transition(transition: dict) -> float:
         headers = {"apikey": API_KEY}
 
         response = requests.request("GET", url, headers=headers, data=payload)
-        print(response.json())
-        print(response.json()["result"])
         return response.json()["result"]
     else:
         return transition["operationAmount"]["amount"]
--- src/masks.py
+++ src/masks.py
@@ -3,7 +3,7 @@ from typing import Union
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("C:/Users/1/PycharmProjects/BankVidjet/logs/masks.log", "w", encoding="utf-8")
+file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
